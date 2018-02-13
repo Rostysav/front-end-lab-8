4c2664295c747f77743e8bf97fd4d4066e6b1b8f
Rostyslav Korin <rostyslav4yk@gmail.com>
2018-02-13 16:24:38 +0200

    Update task2.js

--- homework_07/js/task2.js
+++ homework_07/js/task2.js
@@ -5,13 +5,14 @@ var x;
 var prise = 10;
 var maxChooseNumber = 5;
 var maxAttachment = 3;
+var secondMaxNumbers = maxChooseNumber * 2;
 
 if( play == true ) {
 	while(game) {
 		var randomNumber = Math.floor(Math.random() * 5 );
 		do {
 			x =  prompt(`Choose number from 0 to ${maxChooseNumber} \nAttachment left: ${maxAttachment} \nPrise: ${prise}$`);
-		} while(isNaN(x) || x > 100 || x < 1);
+		} while(isNaN(x) || x > maxChooseNumber || x < 0);
 		if ( x == randomNumber ) {
 			console.log('You win ' + prise + '$');
 			alert(`You win ` + prise + '$')
@@ -27,8 +28,8 @@ if( play == true ) {
 		} else {
 			alert('try one more time');
 			do {
-				x =  prompt(`Choose number from 0 to ${maxChooseNumber} \nAttachment left: ${maxAttachment} \nPrise: ${prise / 2}$`);
-			} while(isNaN(x) || x > 100 || x < 1);
+				x =  prompt(`Choose number from 0 to ${maxChooseNumber} \nAttachment left: ${maxAttachment-1} \nPrise: ${prise / 2}$`);
+			} while(isNaN(x) || x > maxChooseNumber || x < 0);
 			if ( x == randomNumber ) {
 				console.log('You win ' + prise / 2 + '$');
 				alert(`You win ` + prise / 2 + '$')
@@ -44,8 +45,8 @@ if( play == true ) {
 			} else {
 				alert('try one more time');
 				do {
-					x =  prompt(`Choose number from 0 to ${maxChooseNumber} \nAttachment left: ${maxAttachment} \nPrise: ${prise / 5}$`);
-				} while(isNaN(x) || x > 100 || x < 1);
+					x =  prompt(`Choose number from 0 to ${maxChooseNumber} \nAttachment left: ${maxAttachment-2} \nPrise: ${prise / 5}$`);
+				} while(isNaN(x) || x > maxChooseNumber || x < 0);
 				if ( x == randomNumber ) {
 					console.log('You win ' + prise / 5 + '$');
 					alert(`You win ` + prise / 5 + '$')
@@ -66,6 +67,7 @@ if( play == true ) {
 						prise = prise * 3;
 						maxChooseNumber = maxChooseNumber * 2;
 						randomNumber = Math.floor(Math.random() * 10 );
+						maxChooseNumber = secondMaxNumbers;
 					} else {
 						console.log(' Thank you for game');
 						game = false;
