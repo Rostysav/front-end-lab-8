a5a19b4d2a8ef4bc561006206afaf93261445bc2
Rostyslav Korin <rostyslav4yk@gmail.com>
2018-02-13 15:22:57 +0200

    taks 7 fixed

--- homework_07/js/task2.js
+++ homework_07/js/task2.js
@@ -1,91 +1,75 @@
 var play = confirm('Do you want to play game ?');
 var randomNumber = Math.floor(Math.random() * 5 );
+var game = true;
 var x;
 var prise = 10;
-var attachment = 3;
-var pattern = /^[\s]+$/
+var maxChooseNumber = 5;
+var maxAttachment = 3;
+var pattern = /^[\s]+$/;
 
 
-function chekNumber() {
-	if (play == false) {
+if( play == true ) {
+	while(game) {
+		var randomNumber = Math.floor(Math.random() * 5 );
+		x = prompt(`Choose number from 0 to ${maxChooseNumber} \nAttachment left: ${maxAttachment} \nPrise: ${prise}$`);
+		if ( x == randomNumber ) {
+			console.log('You win ' + prise + '$');
+			alert(`You win ` + prise + '$')
+			play = confirm('Do you want to play game one more time ?');
+			if ( play == true ) {
+				game = true;
+				prise = prise * 3;
+				maxChooseNumber = maxChooseNumber * 2;
+			} else {
+				console.log(' Thank you for game');
+				game = false;
+			}
+		} else {
+			alert('try one more time');
+			x = prompt(`Choose number from 0 to 5 \nAttachment left: ${maxAttachment - 1} \nPrise: ${prise / 2}$`);
+			if ( x == randomNumber ) {
+				console.log('You win ' + prise / 2 + '$');
+				alert(`You win ` + prise / 2 + '$')
+				play = confirm('Do you want to play game one more time ?');
+				if ( play == true ) {
+					game = true;
+					prise = prise * 3;
+					maxChooseNumber = maxChooseNumber * 2;
+				} else {
+					console.log(' Thank you for game');
+					game = false;
+				}
+			} else {
+				alert('try one more time');
+				x = prompt(`Choose number from 0 to 5 \nAttachment left: ${maxAttachment - 2} \nPrise: ${(prise / 5)}$`);
+				if ( x == randomNumber ) {
+					console.log('You win ' + prise / 5 + '$');
+					alert(`You win ` + prise / 5 + '$')
+					play = confirm('Do you want to play game one more time ?');
+				if ( play == true ) {
+					game = true;
+					prise = prise * 3;
+					maxChooseNumber = maxChooseNumber * 2;
+				} else {
+					console.log(' Thank you for game');
+					game = false;
+				}
+				} else {
+					alert('You Loose');
+					play = confirm('Do you want to play game one more time ?');
+					if ( play == true ) {
+						game = true;
+						prise = prise * 3;
+						maxChooseNumber = maxChooseNumber * 2;
+					} else {
+						console.log(' Thank you for game');
+						game = false;
+					}
+				}
+			} 
+			
+		}
+	}
+} else {
 	console.log('You did not become a millionaire');
-	return;
-	} else if (play == true) {
-		console.log('Start game');
-		firstChek();
-	}
 }
-chekNumber();
-
-
-function firstChek() {
-	x  = prompt(`Choose number from 0 to 5 \nAttachment left: ${attachment} \nPrise: ${prise}$`);
-	if ( (x != +x) || (x < 0) || (x > 5) || x.length == 0 || pattern.test(x)) {
-		alert('Please enter number from 0 to 5');
-		firstChek();
-	} else if (x != randomNumber) {
-		alert('try one more time');
-		secondChek();
-	} else {
-		console.log('You win ' + prise + '$');
-	}
-}
-
-function secondChek() {
-	x  = prompt(`Choose number from 0 to 5 \nAttachment left: ${attachment - 1} \nPrise: ${prise / 2}$`);
-	if ( (x != +x) || (x < 0) || (x > 5) || x.length == 0 || pattern.test(x) ) {
-		alert('Please enter number from 0 to 5');
-		secondChek();
-	}
-	else if (x != randomNumber) {
-		alert('try one more time');
-		thirdChek();
-	} else {
-			console.log('You win ' + prise / 2 + '$');
-	}
-}
-
-function thirdChek() {
-	x  = prompt(`Choose number from 0 to 5 \nAttachment left: ${attachment - 2} \nPrise: ${prise / prise}$`);
-	if ( (x != +x) || (x < 0) || (x > 5) || x.length == 0 || pattern.test(x)) {
-		alert('Please enter number from 0 to 5');
-		thirdChek();
-	}
-	else if (x != randomNumber) {
-		alert('you loose');
-		play = confirm('Do you want to play one more time ?');
-		chekNumber();
-	} else {
-			console.log('You win ' + prise / 10 + '$');
-			play = confirm('Do you want to play one more time ?');
-			chekNumber();
-	}
-}
-
-console.log(randomNumber);
-
-
-
-
-
-// var play = confirm('Do you want to play game ?');
-// var randomNumber = Math.floor(Math.random() * 5 );
-// var x;
-// var prise = 10;
-// var i = 3;
-// var pattern = /^[\s]+$/
-
-// if ( play == false) {
-// 	console.log('You did not become a millionaire');
-// } else {
-// 	x = prompt(`Choose number from 0 to 5 \nAttachment left: ${i} \nPrise: ${prise} `);
-	
-// 	if ( x < 0 || x > 5 || x.length == 0 || x != +x || pattern.test(x) ) {
-// 		alert('Put NATURAL number from 0 to 5');
-// 	}
-// 	if ( x == randomNumber ) {
-// 		console.log('You win 10$');
-// 	}
-// }
-
-// console.log(randomNumber);
