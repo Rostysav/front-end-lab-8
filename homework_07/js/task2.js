5dbcd79b0e0b7816ea633e2d9cb1ab61d34e5392
Rostyslav Korin <rostyslav4yk@gmail.com>
2018-02-13 16:16:15 +0200

    last commit

--- homework_07/js/task2.js
+++ homework_07/js/task2.js
@@ -1,17 +1,9 @@
-var play = confirm('Do you want to play game ?');
-var randomNumber = Math.floor(Math.random() * 5 );
-var game = true;
-var x;
-var prise = 10;
-var maxChooseNumber = 5;
-var maxAttachment = 3;
-var pattern = /^[\s]+$/;
-
-
 if( play == true ) {
 	while(game) {
 		var randomNumber = Math.floor(Math.random() * 5 );
-		x = prompt(`Choose number from 0 to ${maxChooseNumber} \nAttachment left: ${maxAttachment} \nPrise: ${prise}$`);
+		do {
+			x =  prompt(`Choose number from 0 to ${maxChooseNumber} \nAttachment left: ${maxAttachment} \nPrise: ${prise}$`);
+		} while(isNaN(x) || x > 100 || x < 1);
 		if ( x == randomNumber ) {
 			console.log('You win ' + prise + '$');
 			alert(`You win ` + prise + '$')
@@ -26,7 +18,9 @@ if( play == true ) {
 			}
 		} else {
 			alert('try one more time');
-			x = prompt(`Choose number from 0 to 5 \nAttachment left: ${maxAttachment - 1} \nPrise: ${prise / 2}$`);
+			do {
+				x =  prompt(`Choose number from 0 to ${maxChooseNumber} \nAttachment left: ${maxAttachment} \nPrise: ${prise / 2}$`);
+			} while(isNaN(x) || x > 100 || x < 1);
 			if ( x == randomNumber ) {
 				console.log('You win ' + prise / 2 + '$');
 				alert(`You win ` + prise / 2 + '$')
@@ -41,7 +35,9 @@ if( play == true ) {
 				}
 			} else {
 				alert('try one more time');
-				x = prompt(`Choose number from 0 to 5 \nAttachment left: ${maxAttachment - 2} \nPrise: ${(prise / 5)}$`);
+				do {
+					x =  prompt(`Choose number from 0 to ${maxChooseNumber} \nAttachment left: ${maxAttachment} \nPrise: ${prise / 5}$`);
+				} while(isNaN(x) || x > 100 || x < 1);
 				if ( x == randomNumber ) {
 					console.log('You win ' + prise / 5 + '$');
 					alert(`You win ` + prise / 5 + '$')
